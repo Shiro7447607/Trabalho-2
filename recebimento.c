fe68434a0dec00c6449aeb95ac0ad3e009916f64
55socram55 <marcosnery.comp@gmail.com>
2018-04-19 09:40:35 -0300

    FuncaoReceberImagem

--- recebimento.c
+++ recebimento.c
@@ -8,19 +8,59 @@ Alunos-Matrícula:
 #include <string.h>
 #include <stdlib.h>
 
+
+int** receber(char arquivo[]);
+
 int main () {
         
+
+
+   // printf("%d %d\n",row,column);
+    //printf("%d",imagem[row-2][1024]);
+   // printf("%d ",imagem[1024][1024]);
+          
+   //char nome[] = "grass_01.txt";
+   int **imagem;
+   imagem = receber("grass_01.txt");
+   printf("%d ",imagem[1024][1024]);
+ 
+ 
+    
+    
+   return 0;
+}
+
+int** receber (char arquivo[]){
+    
+    int** imagem;
+    
+    
     FILE *fp;
-    if((fp=fopen("asphalt_01.txt", "r"))==NULL) {
+    if((fp=fopen(arquivo, "r"))==NULL) {
         printf("Cannot open file.\n");
         exit(1);
     }
     
     int n,i;
     char c;
-    int **myarr;
-    myarr = (int**)malloc(1*sizeof(int*));
-    myarr[0] = (int*)malloc(1*sizeof(int));
+    
+    
+    
+    if((imagem = (int**)malloc(1*sizeof(int*)))==NULL){
+        
+            printf("Erro na alocacao");
+            exit(1);
+        }
+    
+    
+    if((imagem[0] = (int*)malloc(1*sizeof(int)))==NULL){
+         
+           printf("Erro na alocacao"); 
+           exit(1);
+        }
+    
+    
+    
     
    // int * ar;
     //ar = (int*)malloc(1*sizeof(int));
@@ -33,36 +73,42 @@ int main () {
     
         fscanf(fp,"%d%c",&n,&c);
         
-        myarr[row][column] = n;
+        imagem[row][column] = n;
         aux++;
         
         if(c=='\n'){
             
             
             row++;
-            myarr = (int**)realloc(myarr,(row+1)*sizeof(int*));
-            myarr[row] = (int*)malloc(1*sizeof(int));
+            if((imagem = (int**)realloc(imagem,(row+1)*sizeof(int*)))==NULL){
+                
+                  printf("Erro na alocacao"); 
+                  exit(1);
+                
+                }
+            if((imagem[row] = (int*)malloc(1*sizeof(int)))==NULL){
+                
+                    printf("Erro na alocacao");
+                    exit(1);
+                }
             column=0;
             aux=0;
             continue;
         }
         //count++;
-        //myarr[row] = (int*)realloc(myarr[row],(column+1)*sizeof(int));
-        //myarr[row][column]=n;
+        //imagem[row] = (int*)realloc(imagem[row],(column+1)*sizeof(int));
+        //imagem[row][column]=n;
         
         column++;
-        myarr[row] = (int*)realloc(myarr[row],(column+1)*sizeof(int));
+        if((imagem[row] = (int*)realloc(imagem[row],(column+1)*sizeof(int)))==NULL){
+            
+                printf("Erro na alocacao");
+                exit(1);
+              
+            }
     
         
     }
     
-
-   // printf("%d %d\n",row,column);
-    //printf("%d",myarr[row-2][1024]);
-    printf("%d ",myarr[1024][1024]);
-        
- 
-    
-    
-   return 0;
+    return imagem;
 }
